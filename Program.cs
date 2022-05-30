bea6fca79407f217542a05c1ad5c2ed1e4154c65
gotikym <gotikym9@mail.ru>
2022-05-30 14:37:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] wallet = new int[0];
+        string userInput;
+        int number;
+        int sum = 0;
+        Console.WriteLine("Для входа в меню - нажмите нажмите любую клавишу\nДля завершения программы введите exit");
+
+        while (Console.ReadLine() != "exit")
+        {
+            Console.WriteLine("добавить числа: 1\nCложить всё что было введено - sum");
+            userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("Введите число: ");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    int[] extensionWallet = new int[wallet.Length + 1];
+
+                    for (int i = 0; i < wallet.Length; i++)
+                    {
+                        extensionWallet[i] = wallet[i];
+                    }
+
+                    extensionWallet[extensionWallet.Length - 1] = number;
+                    wallet = extensionWallet;
+                    Console.Clear();
+                    Console.WriteLine("Для входа в меню - нажмите нажмите любую клавишу, для завершения программы введите exit");
+                    break;
+
+                case "sum":
+
+                    for (int i = 0; i < wallet.Length; i++)
+                    {
+                        sum += wallet[i];
+                    }
+
+                    Console.Clear();
+                    Console.WriteLine("Сумма всех чисел равна: " + sum);
+                    Console.WriteLine("Для входа в меню - нажмите нажмите любую клавишу, для завершения программы введите exit");
+                    break;
+            }
+        }
+    }
+}
